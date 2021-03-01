59f91fe6e4ae55f214e03de208ab18995415927d
Jesse <jesseweijapan@gmail.com>
2021-03-01 10:59:23 +0900

    Code generator and make boilerplate for staffing and lclean and implement header effects

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,26 @@
 $(function () {
-    
+    var $w = $(window);
+    // header
+    var $headers = $(".header");
+    if ($w.scrollTop() > 500) {
+        // display header with bg
+        $headers.each(function () {
+            if (!$(this).hasClass("scroll")) {
+                $(this).addClass(".scroll");
+            }
+        });
+    }
+    $w.scroll(function () {
+        $headers.each(function () {
+            if ($w.scrollTop() > 500) {
+                // display header with bg
+                if (!$(this).hasClass("scroll")) {
+                    $(this).addClass("scroll");
+                }
+            } else {
+                $(this).removeClass("scroll");
+            }
+        });
+        
+    });
 });
